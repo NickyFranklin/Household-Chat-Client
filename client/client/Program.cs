e12ae2e69989a1e0b51b2c127da7a1e96e255f91
NickyFranklin <114303762+NickyFranklin@users.noreply.github.com>
2024-06-14 14:39:39 -0400

    Created both client and server programs
    Created both client and server programs and allow them to connect to each other

---
+++ client/client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+byte[] bytes = new byte[1024];
+
+try {
+    IPHostEntry host = Dns.GetHostEntry("localhost");
+    IPAddress ipAddr = host.AddressList[0];
+    IPEndPoint remoteEndPoint = new IPEndPoint(ipAddr, 11000);
+
+    Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+    try {
+        sender.Connect(remoteEndPoint);
+
+        Console.WriteLine("Socket connected to server");
+
+        byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+
+        int bytesSent = sender.Send(msg);
+        int bytesReceived = sender.Receive(bytes);
+        Console.WriteLine("Echoed test = {0}", Encoding.ASCII.GetString(bytes, 0, bytesReceived));
+
+        sender.Shutdown(SocketShutdown.Both);
+        sender.Close();
+    }
+
+    catch (ArgumentNullException ane) {
+        Console.WriteLine("ArgumentNullExcept : {0}", ane.ToString());
+    }
+
+    catch (SocketException se) {
+        Console.WriteLine("Socket Exception : {0}", se.ToString());
+    }
+
+    catch (Exception e) {
+        Console.WriteLine("Unexpected Exception : {0}", e.ToString());
+    }
+}
+
+catch (Exception e) {
+    Console.WriteLine(e.ToString());
+}
---
+++ server/Chat-Server/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+//Sets up struct/class with server IP and Port information
+IPHostEntry host = Dns.GetHostEntry("localhost");
+IPAddress ipAddr = host.AddressList[0];
+IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11000); //Tutorial did 11000, I hope this just gives a random port
+
+try {
+
+    //Create Socket that uses TCP
+    Socket listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+    //Bind Socket with server info
+    listener.Bind(localEndPoint);
+
+    //Listen to certain number of requests at once
+    listener.Listen(1);
+
+    Console.WriteLine("Waiting for a connection");
+    Socket handler = listener.Accept();
+
+    string? data = null;
+    byte[]? bytes = null;
+
+    while(true) {
+        bytes = new byte[1024];
+        int bytesReceived = handler.Receive(bytes);
+        data += Encoding.ASCII.GetString(bytes, 0, bytesReceived);
+        if(data.IndexOf("<EOF>") > -1) {
+            break;
+        }
+    }
+
+    Console.WriteLine("Text Received : {0}", data);
+
+    byte[] msg = Encoding.ASCII.GetBytes(data);
+    handler.Send(msg);
+    handler.Shutdown(SocketShutdown.Both);
+    handler.Close();
+}
+
+catch (Exception e) {
+    Console.WriteLine(e.ToString());
+}
+
+Console.WriteLine("\nPress any key to continue");
+Console.ReadKey();
+Console.Write("\n");

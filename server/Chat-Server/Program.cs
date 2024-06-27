4ba2d35ba24108004a9733e8784a5455cbdfc6af
NickyFranklin <114303762+NickyFranklin@users.noreply.github.com>
2024-06-27 03:26:39 -0400

    Multiple clients allowed

--- server/Chat-Server/Program.cs
+++ server/Chat-Server/Program.cs
@@ -8,100 +8,110 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-//Sets up struct/class with server IP and Port information
-IPHostEntry host = Dns.GetHostEntry("localhost");
-IPAddress ipAddr = host.AddressList[0];
-IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 0); //Tutorial did 11000, I hope this just gives a random port
+public class Program {
 
-try {
-    //Create Socket that uses TCP
-    Socket listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+    public static void Main() {
+        //Sets up struct/class with server IP and Port information
+        IPHostEntry host = Dns.GetHostEntry("localhost");
+        IPAddress ipAddr = host.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 0); //Tutorial did 11000, I hope this just gives a random port
 
-    //Bind Socket with server info
-    listener.Bind(localEndPoint);
-    Console.WriteLine("Port " + ((IPEndPoint)listener.LocalEndPoint).Port);
+        try {
+            //Create Socket that uses TCP
+            Socket listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-    //Listen to certain number of requests at once
-    listener.Listen(1024);
+            //Bind Socket with server info
+            listener.Bind(localEndPoint);
+            Console.WriteLine("Port " + ((IPEndPoint)listener.LocalEndPoint).Port);
 
-    Console.WriteLine("Waiting for a connection");
+            //Listen to certain number of requests at once
+            listener.Listen(1024);
+
+            Console.WriteLine("Waiting for a connection");
+
+            while(true) {
+                Thread thread1 = new Thread(ProgLoop);
+                thread1.Start(listener.Accept());
+            }
+
+        }
+
+        catch (Exception e) {
+            Console.WriteLine(e.ToString());
+        }
+    }
 
 
-    Socket handler = listener.Accept();
+    //Read a message from the server and return it to the user
+    public static List<byte> readMessage(Socket handler, ref bool msgReceived) {
+        List<byte> buffer = new();
 
-    while(true) {
-        bool msgReceived = false;   
-        var buffer = readMessage(handler, ref msgReceived);
+        byte[] currByte = new byte[1];
+        int byteCounter = handler.Receive(currByte, currByte.Length, SocketFlags.None);
+        while(handler.Available > 0) {
+            if (byteCounter.Equals(1)) {
+                buffer.Add(currByte[0]);
+            }
+                
+            msgReceived = true;
+            currByte = new byte[1];
+            byteCounter = handler.Receive(currByte, currByte.Length, SocketFlags.None);
 
-        if(msgReceived) {
-            string message = String.Join("", System.Text.Encoding.ASCII.GetString(buffer.ToArray()).ToCharArray());
-            Console.WriteLine($"Here is the command that was received: {message}");
+            if (handler.Available <= 0) {
+                buffer.Add(currByte[0]);
+            }
+        }
+
+        return buffer;
+    }
+
+    //Send a message to the server and return the message that was sent
+    public static string sendMessage(Socket handler, string message) {
+        #pragma warning disable CS8604 // Possible null reference argument.
+            byte[] msg = Encoding.ASCII.GetBytes(message);
+        #pragma warning restore CS8604 // Possible null reference argument.
+        int bytesSent = handler.Send(msg);
+
+        return message;
+    }
+
+    static void ProgLoop(object handler) {
+        while(true) {
+            bool msgReceived = false;   
+            var buffer = readMessage((Socket) handler, ref msgReceived);
+
+            if(msgReceived) {
+                string message = String.Join("", System.Text.Encoding.ASCII.GetString(buffer.ToArray()).ToCharArray());
+                Console.WriteLine($"Here is the command that was received: {message}");
         
 
-            if(message.Equals("leave")) {
-                sendMessage(handler, "104 LEAVING");
-                handler.Shutdown(SocketShutdown.Both);
-                handler.Close();
-                break;
-            }
+                if(message.Equals("leave")) {
+                    sendMessage((Socket) handler, "104 LEAVING");
+                    ((Socket) handler).Shutdown(SocketShutdown.Both);
+                    ((Socket) handler).Close();
+                    break;
+                }
 
-            //Dont care if help gets sent
-            else if(message.Equals("help")) {
-                Console.Write("Commands:\n\twrite: tell the server you want to send a message\n\thelp: get a list of commands\n\tleave: leave the program\n");
-                sendMessage(handler, "312 HELP PRINTED");
-            }
+                //Dont care if help gets sent
+                else if(message.Equals("help")) {
+                    Console.Write("Commands:\n\twrite: tell the server you want to send a message\n\thelp: get a list of commands\n\tleave: leave the program\n");
+                    sendMessage((Socket) handler, "312 HELP PRINTED");
+                }
 
-            //let the user send a message
-            else if(message.Equals("write")) {
-                sendMessage(handler, "205 WRITE START");
-                buffer = readMessage(handler, ref msgReceived);
-                sendMessage(handler, "200 OK");
-                message = String.Join("", System.Text.Encoding.ASCII.GetString(buffer.ToArray()).ToCharArray());
-                Console.WriteLine($"Here is the Message that was received: {message}");
-            }
+                //let the user send a message
+                else if(message.Equals("write")) {
+                    sendMessage((Socket) handler, "205 WRITE START");
+                    buffer = readMessage((Socket) handler, ref msgReceived);
+                    sendMessage((Socket) handler, "200 OK");
+                    message = String.Join("", System.Text.Encoding.ASCII.GetString(buffer.ToArray()).ToCharArray());
+                    Console.WriteLine($"Here is the Message that was received: {message}");
+                }
 
-            else {
-                sendMessage(handler, "505 INVALID COMMAND: TYPE help FOR A LIST OF COMMANDS");
+                else {
+                    sendMessage((Socket) handler, "505 INVALID COMMAND: TYPE help FOR A LIST OF COMMANDS");
+                }
             }
         }
     }
 
-}
-
-catch (Exception e) {
-    Console.WriteLine(e.ToString());
-}
-
-
-//Read a message from the server and return it to the user
-List<byte> readMessage(Socket handler, ref bool msgReceived) {
-    List<byte> buffer = new();
-
-    byte[] currByte = new byte[1];
-    int byteCounter = handler.Receive(currByte, currByte.Length, SocketFlags.None);
-    while(handler.Available > 0) {
-        if (byteCounter.Equals(1)) {
-            buffer.Add(currByte[0]);
-        }
-                
-        msgReceived = true;
-        currByte = new byte[1];
-        byteCounter = handler.Receive(currByte, currByte.Length, SocketFlags.None);
-
-        if (handler.Available <= 0) {
-            buffer.Add(currByte[0]);
-        }
-    }
-
-    return buffer;
-}
-
-//Send a message to the server and return the message that was sent
-string sendMessage(Socket handler, string message) {
-    #pragma warning disable CS8604 // Possible null reference argument.
-        byte[] msg = Encoding.ASCII.GetBytes(message);
-    #pragma warning restore CS8604 // Possible null reference argument.
-    int bytesSent = handler.Send(msg);
-
-    return message;
 }
--- server/Chat-Server/obj/Debug/net8.0/Chat-Server.AssemblyInfo.cs
+++ server/Chat-Server/obj/Debug/net8.0/Chat-Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chat-Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2efb325cb1a8c66a8a833a87a19da83f86f51ef7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b290becaab340268652ab290bf0a6f0d43eaaac6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chat-Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chat-Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
